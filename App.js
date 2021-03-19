974a04667272c475d4263b86a223da7a81aa8e2c
Malek Ben Dali <bendali.malekk@gmail.com>
2021-03-19 16:31:22 +0100

    added fonts

--- App.js
+++ App.js
@@ -103,6 +103,7 @@ const styles = StyleSheet.create({
     marginTop: 8,
     fontSize: 18,
     fontWeight: '400',
+    fontFamily: 'Montserrat-Regular',
   },
   highlight: {
     fontWeight: '700',
