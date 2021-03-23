425d4e3cbd53935582a5dab60622f45317ec52d3
Malek Ben Dali <bendali.malekk@gmail.com>
2021-03-23 16:05:13 +0100

    factoring the project

--- src/components/search.js
+++ src/components/search.js
@@ -2,26 +2,34 @@ import React from 'react';
 import {
   View,
   TextInput,
-  Button,
   StyleSheet,
   FlatList,
   Text,
+  TouchableOpacity,
+  Dimensions,
 } from 'react-native';
 import {filmsData} from '../helpers';
+import FilmItem from './filmItem';
 
 const search = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <TextInput style={styles.inputStyle} placeholder="Film Title" />
-      <Button style={styles.container} title="Search" onPress={() => {}} />
+      <TouchableOpacity
+        style={styles.searchButton}
+        title="Search"
+        onPress={() => {}}>
+        <Text style={styles.searchButtonText}>Search</Text>
+      </TouchableOpacity>
       <FlatList
         data={filmsData}
         keyExtractor={item => item.id.toString()}
-        renderItem={({item}) => <Text>{item.title}</Text>}
+        renderItem={({item}) => <FilmItem />}
       />
     </View>
   );
 };
+const windowWidth = Dimensions.get('window').width;
 const styles = StyleSheet.create({
   inputStyle: {
     marginHorizontal: 5,
@@ -29,10 +37,23 @@ const styles = StyleSheet.create({
     borderColor: '#000',
     borderWidth: 1,
     paddingLeft: 10,
-    fontFamily: 'Montserrat-Regular',
+  },
+  searchButton: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+    height: 50,
+    width: windowWidth * 0.6,
+    borderRadius: 99,
+    backgroundColor: '#0d98ba',
   },
   container: {
-    backgroundColor: '#000',
+    padding: 10,
+  },
+  searchButtonText: {
+    fontFamily: 'Montserrat-Bold',
+    color: '#fff',
   },
 });
 export default search;
