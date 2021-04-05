26b9b87ce1ddc257b153a9ba39857805fd6b6f40
Malek Ben Dali <bendali.malekk@gmail.com>
2021-04-05 10:11:34 +0100

    added redux-persist and AsyncStorage

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 import Navigation from './src/navigation/navigation';
 import {Provider} from 'react-redux';
-import Store from './src/store/configureStore';
+import {persistor, store} from './src/store/configureStore';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const App = () => (
-  <Provider store={Store}>
-    <StatusBar backgroundColor="#0d98ba" />
-    <Navigation />
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <StatusBar backgroundColor="#0d98ba" />
+      <Navigation />
+    </PersistGate>
   </Provider>
 );
 
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,7 +2,18 @@ import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import toggleFavorite from './Reducers/ToggleFavorite';
 import avatarReducer from './Reducers/AvatarReducer';
+import AsyncStorage from '@react-native-community/async-storage';
+import {persistReducer, persistStore} from 'redux-persist';
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
 const reducers = combineReducers({toggleFavorite, avatarReducer});
 
-export default createStore(reducers, compose(applyMiddleware(thunk)));
+const persistStateReducer = persistReducer(persistConfig, reducers);
+
+const store = createStore(persistStateReducer, compose(applyMiddleware(thunk)));
+const persistor = persistStore(store);
+
+export {persistor, store};
